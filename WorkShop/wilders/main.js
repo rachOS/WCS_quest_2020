2cc5e0c6bd5e7747892e95443ebebd0206dc6cd6
rachOS <g.chamekh@e-nautia.com>
2020-03-25 16:17:03 +0100

    update workshop nodejs-intro

---
+++ WorkShop/wilders/main.js
@@ -0,0 +1,8 @@
+const chalk = require('chalk')
+
+const wilders1 =  chalk.cyan("Jwana");
+const wilders2 =  chalk.magenta("Enzo");
+const wilders3 =  chalk.yellow("Emeline");
+const wilders4 =  chalk.white("Grégory");
+
+console.log(wilders1, wilders2, wilders3, wilders4)
