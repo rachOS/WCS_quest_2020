0bd9c100b0c11ee20fc4bef37eb52dee503ae950
rachOS <g.chamekh@e-nautia.com>
2020-03-26 21:06:42 +0100

    create workshop Avatar the Simpsons

---
+++ WorkShop/react_props/my-app/src/components/Avatar.js
@@ -0,0 +1,17 @@
+/* Dans une application React, créer le composant Avatar qui prend comme props :
+image: l’url de l’image à afficher
+firstName: le prénom à afficher 
+lastName: le nom à afficher en majuscule (même si le nom passé en props est en minuscule)
+Appeler ce composant dans App.js au moins 3 fois avec des valeurs différentes
+Utilise le spread operator avec le composant Avatar pour afficher les valeurs de l’objet suivant:
+ */
+
+import React from 'react'
+
+const Avatar = () => {
+    image : 
+    firtName :
+    lastName : 
+}
+
+export default Avatar
