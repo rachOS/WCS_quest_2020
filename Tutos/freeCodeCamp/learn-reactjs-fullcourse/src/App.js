a023cfc0d4bb6a6baf18d2fcac4517c9b02876a9
rachOS <g.chamekh@e-nautia.com>
2020-04-05 21:34:05 +0200

    starting freecodecamp tuto

---
+++ Tutos/freeCodeCamp/learn-reactjs-fullcourse/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1 className="App-link"><a href="https://www.youtube.com/watch?v=DLX62G4lc44&feature=emb_logo">freeCodeCamp tutorial from App</a></h1>
+        <h2>paused @ 16:26</h2>
+    </div>
+  );
+}
+
+export default App;
